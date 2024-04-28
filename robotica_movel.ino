e97540fac7cc150b96713a7f6ec1369094a1bf9a
Turra <joao.t06@hotmail.com>
2024-04-28 19:40:18 -0300

    Add file headers and it's license

--- robotica_movel.ino
+++ robotica_movel.ino
@@ -1,3 +1,31 @@
+/**
+ * @file robotica_movel.ino
+ * 
+ * @author João Pedro Verona Turra
+ * 
+ * @brief Arduino code for the robot
+ *
+ **************************************************************************
+ * 
+ *  GNU GPL v3.0
+ * 
+ *  Copyright (c) 2024 João Pedro Verona Turra
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ **************************************************************************
+*/
+
 #include "Ultrasonic.h"
 #include "L298N.h"
 #include "IRremote.hpp"
